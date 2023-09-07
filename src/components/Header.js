80b0a47ad0c36d660a70afc70f5b4ac65b6f4fb6
vishnu narayanan <vishnunarayanan2044@gmail.com>
2023-09-07 19:16:17 +0530

    cm

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import headerCss from "./Header.module.css";
 import {Button, Accordion} from 'react-bootstrap';
  
 
+
 const Header = () => {
+
+  const clickHandle = () =>{
+    console.log("click me clicked!!");
+  }
+
+  const doubleClickHandle = (name) =>{
+    alert("Hello " + name)
+  }
+
+  const onKeyDownHandle = (name) =>{
+    alert("Hello " + name)
+  }
+
+  const [colour, setColour] = useState("Yellow");
+  const [count, setCount] = useState(0)
+
+  const incriment = () => {
+    setCount(count +1)
+
+  };
+
+  const decriment = () =>{
+    setCount(count - 1)
+  };
+
+  const changeColour = () =>{
+    setColour("red")
+  }
 
   return (
     <>
 
       <h1>hello world</h1>
-      <Button variant="success">Success</Button>{' '}
+      <Button variant="success" onClick={clickHandle}>Click Me</Button>{' '}
+      <input type='text' onKeyDown={onKeyDownHandle}  />
+      <Button variant="warning" onDoubleClick={() => doubleClickHandle("vishnu")}>On Double click</Button>{' '}
+      <br/>
+      <br/>
+      <h1>my favourite color is {colour}</h1>
+      <Button variant="success" onClick={changeColour} >Click to change the color</Button>{' '}
+      <br/>
+      <br/> 
+      <h1>counter is {count}</h1>
+      <Button onClick={incriment} className='m-2 bg-success' >+</Button>{' '}
+      <Button onClick={decriment} className='m-2 bg-danger' >-</Button>{' '}
       <br/>
       <br/>
 
